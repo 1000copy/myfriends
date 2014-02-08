46c9cf053b8231bd4cc01169f54e16ac39bfb4c8
1000copy@gmail.com <1000copy@gmail.com>
2014-02-08 15:55:35 +0800

    --

--- beeline.js
+++ beeline.js
@@ -1,12 +1,12 @@
 var bee = require("beeline");
 var router = bee.route({ // Create a new router
-    "/cheggit": function(req, res) {
+    "/user/`username`": function(req, res, tokens, values) {
         // Called when req.url === "/cheggit" or req.url === "/cheggit?woo=poo"
         res.writeHeader(200, {"Content-Type": "text/html"});  
-        res.write("Hello World!");  
+        res.write("Hello World!"+ tokens.username);  
         res.end();  
     }    
 });
 // Starts serve with routes defined above:
 require("http").createServer(router).listen(8001);
-console.log("up to run ...")
+console.log("up to run ... http://localhost:8001/")
